812e7e1509c92e4b360a64884fcd312f9657c59c
mbullington <mikebullingtn@gmail.com>
2017-01-15 21:50:43 -0500

    v2.0.0, the 'it works again!' update

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+v2.0.0
+
+The `it works again!` update: 
+
+- A large refactor, so I decided to bump up the major version.
+- Fix embarassing errors with copy-pasted JS code from 1.5 years ago.
+- Add proper RTSP data framing, other than whatever was there.
+- Remove Babel/Gulp entirely, we only use what Node v6.0+ supports as far as ES6.
+- Add yarn.lock for anyone using Yarn.
+- Fix example.
--- examples/wowza.js
+++ examples/wowza.js
@@ -1,4 +1,4 @@
-var RtspClient = require('../dist/rtsp.js').RtspClient;
+var RtspClient = require('../lib').RtspClient;
 
 var client = new RtspClient();
 
@@ -12,13 +12,11 @@ client.connect('rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov
 
 // data == packet.payload, just a small convenient thing
 client.on('data', function(data, packet) {
-  process.stdout.write(data);
+  console.log(packet.id);
 });
 
 // allows you to optionally allow for RTSP logging
 // also allows for you to hook this into your own logging system easily
-/*
 client.on('log', function(data, prefix) {
   console.log(prefix + ': ' + data);
 });
-*/
