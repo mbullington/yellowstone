433c623137672977ca02c8dae8b1e5a678f9cd5e
Michael Bullington <mikebullingtn@gmail.com>
2018-01-02 19:03:13 -0500

    Clean up code even further, fix URL for example, start work on 2.1.1

--- examples/wowza.js
+++ examples/wowza.js
@@ -5,8 +5,8 @@
 const { RtspClient, H264Transport } = require('../lib');
 const fs = require('fs');
 
-const url = 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov';
-const filename = 'video.264';
+const url = 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov';
+const filename = 'bigbuckbunny.264';
 
 const client = new RtspClient();
 
@@ -14,32 +14,30 @@ const client = new RtspClient();
 //   format - string
 //   mediaSource - media portion of the SDP
 //   transport RTP and RTCP channels
-
-client.connect(url, { keepAlive: true }).then((details) => {
+client.connect(url, { keepAlive: true }).then(details => {
   console.log('Connected. Video format is', details.format);
 
   // Open the output file
   if (details.isH264) {
-    const h264 = new H264Transport(client, fs.createWriteStream("bigbuckbunny.264"), details);
+    const h264 = new H264Transport(client, fs.createWriteStream(filename), details);
   }
 
   client.play();
-});
+}).catch(e => console.log(e));
 
 // data == packet.payload, just a small convenient thing
 // data is for RTP packets
-client.on('data', function(channel, data, packet) {
+client.on('data', (channel, data, packet) => {
   console.log('RTP Packet', 'ID=' + packet.id, 'TS=' + packet.timestamp, 'M=' + packet.marker);
 });
 
 // control data is for RTCP packets
-client.on('controlData', function(channel, rtcpPacket) {
+client.on('controlData', (channel, rtcpPacket) => {
   console.log('RTCP Control Packet', 'TS=' + rtcpPacket.timestamp, 'PT=' + rtcpPacket.packetType);
 });
 
 // allows you to optionally allow for RTSP logging
 // also allows for you to hook this into your own logging system easily
-client.on('log', function(data, prefix) {
+client.on('log', (data, prefix) => {
   console.log(prefix + ': ' + data);
 });
-
